33a2c56903f8d7ff1f99d457836f205acf1a1003
Ledi Hildawan <lhildawan@gmail.com>
2019-11-01 11:48:49 +0800

    slider home

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,16 @@
     //     slidesToShow: 4,
     //     slidesToScroll: 1
     // });
+
+    $('.featured-articles__list').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
     
     // const projects = document.querySelectorAll('.project');
     
