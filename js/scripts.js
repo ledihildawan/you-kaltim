05385fa13c479337889c327d9db63e87f11afa93
Ledi Hildawan <lhildawan@gmail.com>
2019-11-11 09:49:28 +0800

    add some pages

--- js/scripts.js
+++ js/scripts.js
@@ -43,8 +43,8 @@
             slidesToShow: 1,
             slidesToScroll: 1,
             dots: true,
-            // autoplay: true,
-            autoplaySpeed: 2000,
+            autoplay: true,
+            autoplaySpeed: 6000,
             arrows: false
         });
     } catch (error) {}
@@ -110,9 +110,16 @@
             mainHeader.classList.add('fixed');
         });
 
-        teamModal.addEventListener('click', () => {
-            teamModal.classList.remove('active');
-            mainHeader.classList.add('fixed');
+        // teamModal.addEventListener('click', () => {
+        //     teamModal.classList.remove('active');
+        //     mainHeader.classList.add('fixed');
+        // });
+
+        document.body.addEventListener('click', (e) => {
+            if (e.target.classList.contains('team-modal')) {
+                teamModal.classList.remove('active');
+                mainHeader.classList.add('fixed');
+            }
         });
         
         // teamCloseIconForm.addEventListener('click', () => {
@@ -268,11 +275,135 @@
     //         upcommingEventsModal.style.overflowY = 'scroll';
     //     });
 
+    // try {
+    //     const mainHeader = document.querySelector('.main-header');
+    //     const upcomingEvents = document.querySelector('.upcoming');
+    //     const upcommingEventsModal = document.querySelector('.upcomming-events-modal');
+    //     const upcommingEventsModalClose = document.querySelector('.upcomming-events-modal__close');
+    //     const showMoreModalProject = document.querySelector('.upcomming-events-modal__showmore');
+    //     const showMoreLabelModalProject = document.querySelector('.upcomming-events-modal__showmore span');
+    //     const showMoreIconModalProject = document.querySelector('.upcomming-events-modal__showmore i');
+
+    //     upcommingEventsModalClose.addEventListener('click', () => {
+    //         upcommingEventsModal.classList.remove('active');
+    //         mainHeader.classList.add('fixed');
+    //     })
+
+    //     upcomingEvents.addEventListener('click', (event) => {
+    //         event.preventDefault();
+    //         mainHeader.classList.remove('fixed');
+    //         upcommingEventsModal.classList.add('active');
+    //     });
+
+    //     document.body.addEventListener('click', (event) => {
+    //         if (event.target.classList.contains('upcomming-events-modal')) {
+    //             upcommingEventsModal.classList.remove('active');
+    //             mainHeader.classList.add('fixed');
+    //         }
+    //     });
+
+    //     showMoreModalProject.addEventListener('click', () => {
+    //         showMoreModalProject.parentElement.classList.toggle('active');
+
+    //         const active = showMoreModalProject.parentElement.classList.contains('active');
+
+    //         if (active) {
+    //             showMoreLabelModalProject.textContent = 'Less';
+    //             showMoreIconModalProject.className = 'fas fa-chevron-up';
+    //         } else {
+    //             showMoreLabelModalProject.textContent = 'More';
+    //             showMoreIconModalProject.className = 'fas fa-chevron-down';
+    //         }
+    //     });
+    // } catch (error) {}
+
+    try {
+        var x, i, j, selElmnt, a, b, c;
+        /* Look for any elements with the class "custom-select": */
+        x = document.getElementsByClassName("custom-select");
+        for (i = 0; i < x.length; i++) {
+        selElmnt = x[i].getElementsByTagName("select")[0];
+        /* For each element, create a new DIV that will act as the selected item: */
+        a = document.createElement("DIV");
+        a.setAttribute("class", "select-selected");
+        a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+        x[i].appendChild(a);
+        /* For each element, create a new DIV that will contain the option list: */
+        b = document.createElement("DIV");
+        b.setAttribute("class", "select-items select-hide");
+        for (j = 1; j < selElmnt.length; j++) {
+            /* For each option in the original select element,
+            create a new DIV that will act as an option item: */
+            c = document.createElement("DIV");
+            c.innerHTML = selElmnt.options[j].innerHTML;
+            c.addEventListener("click", function(e) {
+                /* When an item is clicked, update the original select box,
+                and the selected item: */
+                var y, i, k, s, h;
+                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                h = this.parentNode.previousSibling;
+                for (i = 0; i < s.length; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    for (k = 0; k < y.length; k++) {
+                    y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+                }
+                h.click();
+            });
+            b.appendChild(c);
+        }
+        x[i].appendChild(b);
+        a.addEventListener("click", function(e) {
+            /* When the select box is clicked, close any other select boxes,
+            and open/close the current select box: */
+            e.stopPropagation();
+            closeAllSelect(this);
+            this.nextSibling.classList.toggle("select-hide");
+            this.classList.toggle("select-arrow-active");
+        });
+        }
+
+        function closeAllSelect(elmnt) {
+        /* A function that will close all select boxes in the document,
+        except the current select box: */
+        var x, y, i, arrNo = [];
+        x = document.getElementsByClassName("select-items");
+        y = document.getElementsByClassName("select-selected");
+        for (i = 0; i < y.length; i++) {
+            if (elmnt == y[i]) {
+            arrNo.push(i)
+            } else {
+            y[i].classList.remove("select-arrow-active");
+            }
+        }
+        for (i = 0; i < x.length; i++) {
+            if (arrNo.indexOf(i)) {
+                x[i].classList.add("select-hide");
+            }
+        }
+    }
+
+    /* If the user clicks anywhere outside the select box,
+    then close all select boxes: */
+    document.addEventListener("click", closeAllSelect);
+    } catch (error) {}
+
     try {
         const mainHeader = document.querySelector('.main-header');
-        const upcomingEvents = document.querySelector('.upcoming');
+        const upcomingEvents = document.querySelectorAll('.upcoming');
         const upcommingEventsModal = document.querySelector('.upcomming-events-modal');
         const upcommingEventsModalClose = document.querySelector('.upcomming-events-modal__close');
+        const upcommingEventsModalTitle = document.querySelector('.upcomming-events-modal__title');
+        const upcommingEventsModalPoster = document.querySelector('.upcomming-events-modal__poster img');
+        const upcommingEventsModalDate = document.querySelector('.upcomming-events-modal__date span');
+        const upcommingEventsModalLocation = document.querySelector('.upcomming-events-modal__location span');
+        const upcommingEventsModalContent = document.querySelector('.upcomming-events-modal__content-inner');
         const showMoreModalProject = document.querySelector('.upcomming-events-modal__showmore');
         const showMoreLabelModalProject = document.querySelector('.upcomming-events-modal__showmore span');
         const showMoreIconModalProject = document.querySelector('.upcomming-events-modal__showmore i');
@@ -282,16 +413,27 @@
             mainHeader.classList.add('fixed');
         })
 
-        upcomingEvents.addEventListener('click', (event) => {
-            event.preventDefault();
-            mainHeader.classList.remove('fixed');
-            upcommingEventsModal.classList.add('active');
-        });
+        // upcomingEvents.addEventListener('click', (event) => {
+        //     // event.preventDefault();
+        //     mainHeader.classList.remove('fixed');
+        //     upcommingEventsModal.classList.add('active');
+        // });
 
         document.body.addEventListener('click', (event) => {
             if (event.target.classList.contains('upcomming-events-modal')) {
                 upcommingEventsModal.classList.remove('active');
                 mainHeader.classList.add('fixed');
+            }
+
+            if (event.target.classList.contains('upcomming')) {
+                mainHeader.classList.remove('fixed');
+                upcommingEventsModal.classList.add('active');
+
+                upcommingEventsModalTitle.textContent = event.target.dataset.upcommingTitle;
+                upcommingEventsModalDate.textContent = event.target.dataset.upcommingDate;
+                upcommingEventsModalLocation.textContent = event.target.dataset.upcommingLocation;
+                upcommingEventsModalPoster.src = event.target.dataset.upcommingPoster;
+                upcommingEventsModalContent.innerHTML = decodeURIComponent(event.target.dataset.upcommingContent).replace(/[+]/g, ' ');
             }
         });
 
